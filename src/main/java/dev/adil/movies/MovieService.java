3d64563afe304db245c3ba1143cfa0bdd0bdb318
adilakarkach <adilakarkach@gmail.com>
2024-07-16 23:43:11 +0200

    First Commit

---
+++ src/main/java/dev/adil/movies/MovieService.java
@@ -0,0 +1,25 @@
+package dev.adil.movies;
+
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class MovieService {
+    @Autowired
+    private MovieRepository movieRepository;
+
+    public List<Movie> allMovies(){
+        return movieRepository.findAll();
+    }
+
+    public Optional<Movie> singleMovie(String imdbId) {
+        return movieRepository.findMovieByImdbId(imdbId);
+    }
+}
